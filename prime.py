f1e6ac173c8a8f067e2b2b142d367626e8f39b64
Your Name <you@example.com>
2019-11-19 11:44:57 -0700

    created program that generates first 100 primes

---
+++ prime.py
@@ -0,0 +1,8 @@
+def primes_upto(limit):
+    prime = [True] * limit
+    for n in range(2, limit):
+        if prime[n]:
+            yield n # n is a prime
+            for c in range(n*n, limit, n):
+                prime[c] = False # mark composites
+print(list(primes_upto(542)))
